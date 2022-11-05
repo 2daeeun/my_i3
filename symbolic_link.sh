f625de00092b7322e08a344a25c76f49fce850c4
root <root@debian-BULLSEYE-live-builder-AMD64>
2022-11-06 01:46:13 +0900

    config file -> my PC

---
+++ symbolic_link.sh
@@ -0,0 +1,17 @@
+mv ~/.config/htop ~/.local/share/Trash/files &
+mv ~/.config/i3 ~/.local/share/Trash/files &
+mv ~/.config/i3status ~/.local/share/Trash/files &
+mv ~/.config/nvim ~/.local/share/Trash/files &
+mv ~/.config/ranger ~/.local/share/Trash/files &
+mv ~/.config/terminator ~/.local/share/Trash/files &
+mv ~/.zshrc ~/.local/share/Trash/files &
+mv ~/.p10k.zsh ~/.local/share/Trash/files &
+
+ln -s ~/Documents/github/my_i3/home/.config/htop ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.config/i3 ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.config/i3status ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.config/nvim ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.config/ranger ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.config/terminator ~/.config/ &
+ln -s ~/Documents/github/my_i3/home/.zshrc ~/ &
+ln -s ~/Documents/github/my_i3/home/.p10k.zsh ~/ &
