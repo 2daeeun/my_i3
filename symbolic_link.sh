dce02ecd8406bcd5ef2c16214486c3b6d7d2935c
2daeeun <ghostintheme@naver.com>
2022-11-06 17:52:53 +0900

    ln -s -> ln -sf

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -1,17 +1,17 @@
-mv ~/.config/htop ~/.local/share/Trash/files &
-mv ~/.config/i3 ~/.local/share/Trash/files &
-mv ~/.config/i3status ~/.local/share/Trash/files &
-mv ~/.config/nvim ~/.local/share/Trash/files &
-mv ~/.config/ranger ~/.local/share/Trash/files &
-mv ~/.config/terminator ~/.local/share/Trash/files &
-mv ~/.zshrc ~/.local/share/Trash/files &
-mv ~/.p10k.zsh ~/.local/share/Trash/files &
+sudo rm -rf ~/.config/htop
+sudo rm -rf ~/.config/i3
+sudo rm -rf ~/.config/i3status
+sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.config/ranger
+sudo rm -rf ~/.config/terminator
+sudo rm -rf ~/.zshrc
+sudo rm -rf ~/.p10k.zsh
 
-ln -s ~/Documents/github/my_i3/home/.config/htop ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.config/i3 ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.config/i3status ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.config/nvim ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.config/ranger ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.config/terminator ~/.config/ &
-ln -s ~/Documents/github/my_i3/home/.zshrc ~/ &
-ln -s ~/Documents/github/my_i3/home/.p10k.zsh ~/ &
+sudo ln -sf ~/Documents/github/my_i3/home/.config/htop ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/i3 ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/i3status ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/nvim ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/ranger ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/terminator ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.zshrc ~/
+sudo ln -sf ~/Documents/github/my_i3/home/.p10k.zsh ~/
