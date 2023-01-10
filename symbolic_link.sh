dff50918babd0719878c1a3b9ef1688afe5b9c6f
2daeeun <root@debian-BULLSEYE-live-builder-AMD64>
2023-01-10 12:57:36 +0900

    [+] add autorandr

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -5,6 +5,7 @@ sudo rm -rf ~/.config/nvim
 sudo rm -rf ~/.config/ranger
 sudo rm -rf ~/.config/terminator
 sudo rm -rf ~/.config/xfce4
+sudo rm -rf ~/.config/autorandr
 sudo rm -rf ~/.zshrc
 sudo rm -rf ~/.p10k.zsh
 sudo rm -rf ~/.scrot_clipboard.sh
@@ -16,6 +17,7 @@ sudo ln -sf ~/Documents/github/my_i3/home/.config/nvim ~/.config/
 sudo ln -sf ~/Documents/github/my_i3/home/.config/ranger ~/.config/
 sudo ln -sf ~/Documents/github/my_i3/home/.config/terminator ~/.config/
 sudo ln -sf ~/Documents/github/my_i3/home/.config/xfce4 ~/.config/
+sudo ln -sf ~/Documents/github/my_i3/home/.config/autorandr ~/.config/
 sudo ln -sf ~/Documents/github/my_i3/home/.zshrc ~/
 sudo ln -sf ~/Documents/github/my_i3/home/.p10k.zsh ~/
 sudo ln -sf ~/Documents/github/my_i3/home/.scrot_clipboard.sh ~/
